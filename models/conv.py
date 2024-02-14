03ee53ee61100d4a3f5acbea5544efc541a587a8
Arpit Kapoor <kapoor.arpit97@gmail.com>
2024-02-15 10:40:47 +1100

    Final update

--- models/conv.py
+++ models/conv.py
@@ -1,86 +1,263 @@
+from typing import List, Optional, Tuple, Union
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-class SpectralConv3d(nn.Module):
+import tensorly as tl
+from tensorly.plugins import use_opt_einsum
+from tltorch.factorized_tensors.core import FactorizedTensor
+
+tl.set_backend("pytorch")
+use_opt_einsum("optimal")
+einsum_symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def _contract_dense(x, weight, separable=False):
+    order = tl.ndim(x)
+    # batch-size, in_channels, x, y...
+    x_syms = list(einsum_symbols[:order])
+
+    # in_channels, out_channels, x, y...
+    weight_syms = list(x_syms[1:])  # no batch-size
+
+    # batch-size, out_channels, x, y...
+    if separable:
+        out_syms = [x_syms[0]] + list(weight_syms)
+    else:
+        weight_syms.insert(1, einsum_symbols[order])  # outputs
+        out_syms = list(weight_syms)
+        out_syms[0] = x_syms[0]
+
+    eq = f'{"".join(x_syms)},{"".join(weight_syms)}->{"".join(out_syms)}'
+
+    if not torch.is_tensor(weight):
+        weight = weight.to_tensor()
+
+    return tl.einsum(eq, x, weight)
+
+
+
+
+class SpectralConv(nn.Module):
+    """Generic N-Dimensional Fourier Neural Operator
+
+    Parameters
+    ----------
+    in_channels : int, optional
+        Number of input channels
+    out_channels : int, optional
+        Number of output channels
+    max_n_modes : None or int tuple, default is None
+        Number of modes to use for contraction in Fourier domain during training.
+ 
+        .. warning::
+            
+            We take care of the redundancy in the Fourier modes, therefore, for an input 
+            of size I_1, ..., I_N, please provide modes M_K that are I_1 < M_K <= I_N
+            We will automatically keep the right amount of modes: specifically, for the 
+            last mode only, if you specify M_N modes we will use M_N // 2 + 1 modes 
+            as the real FFT is redundant along that last dimension.
+
+            
+        .. note::
+
+            Provided modes should be even integers. odd numbers will be rounded to the closest even number.  
+
+        This can be updated dynamically during training.
+
+    max_n_modes : int tuple or None, default is None
+        * If not None, **maximum** number of modes to keep in Fourier Layer, along each dim
+            The number of modes (`n_modes`) cannot be increased beyond that.
+        * If None, all the n_modes are used.
+
+    n_layers : int, optional
+        Number of Fourier Layers, by default 4
     """
-    3D Fourier layer. It does FFT, linear transform, and Inverse FFT.
-    """
-    def __init__(self, in_channels, out_channels, modes1, modes2, modes3):
-        super(SpectralConv3d, self).__init__()
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        n_modes,
+        max_n_modes=None,
+        bias=True,
+        n_layers=1,
+        init_std="auto",
+        fft_norm="backward",
+        device=None,
+        dtype=None,
+    ):
+        super().__init__(dtype=dtype, device=device)
+
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.modes1 = modes1 #Number of Fourier modes to multiply, at most floor(N/2) + 1
-        self.modes2 = modes2
-        self.modes3 = modes3
 
-        self.scale = (1 / (in_channels * out_channels))
-        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
-        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
-        self.weights3 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
-        self.weights4 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+        # n_modes is the total number of modes kept along each dimension
+        self.n_modes = n_modes
+        self.order = len(self.n_modes)
 
-    # Complex multiplication
-    def compl_mul3d(self, input, weights):
-        # (batch, in_channel, x,y,t ), (in_channel, out_channel, x,y,t) -> (batch, out_channel, x,y,t)
-        return torch.einsum("bixyz,ioxyz->boxyz", input, weights)
+        if max_n_modes is None:
+            max_n_modes = self.n_modes
+        elif isinstance(max_n_modes, int):
+            max_n_modes = [max_n_modes]
+        self.max_n_modes = max_n_modes
 
-    def forward(self, x):
-        batchsize = x.shape[0]
-        #Compute Fourier coeffcients up to factor of e^(- something constant)
-        x_ft = torch.fft.rfftn(x, dim=[-3,-2,-1])
+        self.n_layers = n_layers
 
-        # Multiply relevant Fourier modes
-        out_ft = torch.zeros(batchsize, self.out_channels, x.size(-3), x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
-        out_ft[:, :, :self.modes1, :self.modes2, :self.modes3] = \
-            self.compl_mul3d(x_ft[:, :, :self.modes1, :self.modes2, :self.modes3], self.weights1)
-        out_ft[:, :, -self.modes1:, :self.modes2, :self.modes3] = \
-            self.compl_mul3d(x_ft[:, :, -self.modes1:, :self.modes2, :self.modes3], self.weights2)
-        out_ft[:, :, :self.modes1, -self.modes2:, :self.modes3] = \
-            self.compl_mul3d(x_ft[:, :, :self.modes1, -self.modes2:, :self.modes3], self.weights3)
-        out_ft[:, :, -self.modes1:, -self.modes2:, :self.modes3] = \
-            self.compl_mul3d(x_ft[:, :, -self.modes1:, -self.modes2:, :self.modes3], self.weights4)
+        if init_std == "auto":
+            init_std = (2 / (in_channels + out_channels))**0.5
+        else:
+            init_std = init_std
 
-        #Return to physical space
-        x = torch.fft.irfftn(out_ft, s=(x.size(-3), x.size(-2), x.size(-1)))
+        if isinstance(fixed_rank_modes, bool):
+            if fixed_rank_modes:
+                # If bool, keep the number of layers fixed
+                fixed_rank_modes = [0]
+            else:
+                fixed_rank_modes = None
+        
+        self.fft_norm = fft_norm
+        factorization = "Dense"
+
+        weight_shape = (in_channels, out_channels, *max_n_modes)
+
+
+        self.weight = nn.ModuleList(
+            [
+                FactorizedTensor.new(
+                    weight_shape,
+                    rank=self.rank,
+                    factorization=factorization,
+                    fixed_rank_modes=fixed_rank_modes
+                )
+                for _ in range(n_layers)
+            ]
+        )
+        for w in self.weight:
+            w.normal_(0, init_std)
+        self._contract = _contract_dense
+
+        if bias:
+            self.bias = nn.Parameter(
+                init_std
+                * torch.randn(*((n_layers, self.out_channels) + (1,) * self.order))
+            )
+        else:
+            self.bias = None
+
+    def _get_weight(self, index):
+        return self.weight[index]
+    
+    @property
+    def n_modes(self):
+        return self._n_modes
+    
+    @n_modes.setter
+    def n_modes(self, n_modes):
+        if isinstance(n_modes, int): # Should happen for 1D FNO only
+            n_modes = [n_modes]
+        else:
+            n_modes = list(n_modes)
+        # The last mode has a redundacy as we use real FFT
+        # As a design choice we do the operation here to avoid users dealing with the +1
+        n_modes[-1] = n_modes[-1] // 2 + 1
+        self._n_modes = n_modes
+
+    def forward(
+        self, x: torch.Tensor, indices=0, output_shape: Optional[Tuple[int]] = None
+    ):
+        """Generic forward pass for the Factorized Spectral Conv
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            input activation of size (batch_size, channels, d1, ..., dN)
+        indices : int, default is 0
+            if joint_factorization, index of the layers for n_layers > 1
+
+        Returns
+        -------
+        tensorized_spectral_conv(x)
+        """
+        batchsize, channels, *mode_sizes = x.shape
+
+        fft_size = list(mode_sizes)
+        fft_size[-1] = fft_size[-1] // 2 + 1  # Redundant last coefficient
+        fft_dims = list(range(-self.order, 0))
+
+        if self.fno_block_precision == "half":
+            x = x.half()
+
+        x = torch.fft.rfftn(x, norm=self.fft_norm, dim=fft_dims)
+        if self.order > 1:
+            x = torch.fft.fftshift(x, dim=fft_dims[:-1])
+
+        out_dtype = torch.cfloat
+        out_fft = torch.zeros([batchsize, self.out_channels, *fft_size],
+                              device=x.device, dtype=out_dtype)
+        starts = [(max_modes - min(size, n_mode)) for (size, n_mode, max_modes) in zip(fft_size, self.n_modes, self.max_n_modes)]
+        slices_w =  [slice(None), slice(None)] # Batch_size, channels
+        slices_w += [slice(start//2, -start//2) if start else slice(start, None) for start in starts[:-1]]
+        slices_w += [slice(None, -starts[-1]) if starts[-1] else slice(None)] # The last mode already has redundant half removed
+        weight = self._get_weight(indices)[slices_w]
+
+        starts = [(size - min(size, n_mode)) for (size, n_mode) in zip(list(x.shape[2:]), list(weight.shape[2:]))]
+        slices_x =  [slice(None), slice(None)] # Batch_size, channels
+        slices_x += [slice(start//2, -start//2) if start else slice(start, None) for start in starts[:-1]]
+        slices_x += [slice(None, -starts[-1]) if starts[-1] else slice(None)] # The last mode already has redundant half removed
+        out_fft[slices_x] = self._contract(x[slices_x], weight, separable=False)
+
+
+        if output_shape is not None:
+            mode_sizes = output_shape
+
+        if self.order > 1:
+            out_fft = torch.fft.fftshift(out_fft, dim=fft_dims[:-1])
+        x = torch.fft.irfftn(out_fft, s=mode_sizes, dim=fft_dims, norm=self.fft_norm)
+
+        if self.bias is not None:
+            x = x + self.bias[indices, ...]
+
         return x
 
+    def get_conv(self, indices):
+        """Returns a sub-convolutional layer from the joint parametrize main-convolution
 
-class SpectralConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, modes1, modes2):
-        super(SpectralConv2d, self).__init__()
-
+        The parametrization of sub-convolutional layers is shared with the main one.
         """
-        2D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
-        """
+        if self.n_layers == 1:
+            Warning("A single convolution is parametrized, directly use the main class.")
 
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.modes1 = modes1 #Number of Fourier modes to multiply, at most floor(N/2) + 1
-        self.modes2 = modes2
+        return SubConv(self, indices)
 
-        self.scale = (1 / (in_channels * out_channels))
-        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
-        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+    def __getitem__(self, indices):
+        return self.get_conv(indices)
 
-    # Complex multiplication
-    def compl_mul2d(self, input, weights):
-        # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
-        return torch.einsum("bixy,ioxy->boxy", input, weights)
 
-    def forward(self, x):
-        batchsize = x.shape[0]
-        #Compute Fourier coeffcients up to factor of e^(- something constant)
-        x_ft = torch.fft.rfft2(x)
+class SubConv(nn.Module):
+    """Class representing one of the convolutions from the mother joint
+    factorized convolution.
 
-        # Multiply relevant Fourier modes
-        out_ft = torch.zeros(batchsize, self.out_channels,  x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
-        out_ft[:, :, :self.modes1, :self.modes2] = \
-            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
-        out_ft[:, :, -self.modes1:, :self.modes2] = \
-            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+    Notes
+    -----
+    This relies on the fact that nn.Parameters are not duplicated:
+    if the same nn.Parameter is assigned to multiple modules, they all point to
+    the same data, which is shared.
+    """
 
-        #Return to physical space
-        x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
-        return x
+    def __init__(self, main_conv, indices):
+        super().__init__()
+        self.main_conv = main_conv
+        self.indices = indices
+
+    def forward(self, x, **kwargs):
+        return self.main_conv.forward(x, self.indices, **kwargs)
+
+    def transform(self, x, **kwargs):
+        return self.main_conv.transform(x, self.indices, **kwargs)
+
+    @property
+    def weight(self):
+        return self.main_conv.get_weight(indices=self.indices)
--- models/fno.py
+++ models/fno.py
@@ -8,55 +8,133 @@ import operator
 from functools import reduce
 from functools import partial
 
-from .conv import SpectralConv3d
+from .conv import SpectralConv
+from .mlp import MLP
 
 
-class FNO3d(nn.Module):
+class FNO(nn.Module):
     """
-    FNO contains 5 Fourier interal operator layers
+    FNO model consisting of a sequence of a lifting, `n` fourier 
+    interal operator layers, and a projection layer
     """
 
-    def __init__(self, modes1, modes2, modes3, width, 
-                 n_blocks=5, in_channel=4, device='cpu'):
-        super(FNO3d, self).__init__()
+    def __init__(self, 
+                 n_modes,
+                 hidden_channels,
+                 in_channels=3,
+                 out_channels=1,
+                 lifting_channels=256,
+                 projection_channels=256,
+                 n_layers=4,
+                 non_linearity=F.gelu,
+                 skip_fno_bias=False,
+                 fft_norm="forward"):
+        
+        super(FNO, self).__init__()
+        self.n_dims = len(n_modes)
 
-        self.modes1 = modes1
-        self.modes2 = modes2
-        self.modes3 = modes3
-        self.width = width
-        self.n_blocks = n_blocks
-        self.upscale = nn.Linear(in_channel, self.width).to(device)
-        self.spectral_conv3d = [SpectralConv3d(self.width, self.width,
-                                               self.modes1, self.modes2, 
-                                               self.modes3).to(device) for _ in range(self.n_blocks)]
-        self.linear = [nn.Conv1d(self.width, self.width, 1).to(device) for _ in range(self.n_blocks)]
+        self._n_modes = n_modes
+        self.hidden_channels = hidden_channels
+        self.lifting_channels = lifting_channels
+        self.projection_channels = projection_channels
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.n_layers = n_layers
+        self.non_linearity = non_linearity
 
-        self.downscale = nn.Linear(self.width, 128).to(device)
-        self.output = nn.Linear(128, 1).to(device)
+        # if lifting_channels is passed, make lifting an MLP
+        # with a hidden layer of size lifting_channels
+        if self.lifting_channels:
+            self.lifting = MLP(
+                in_channels=in_channels,
+                out_channels=self.hidden_channels,
+                hidden_channels=self.lifting_channels,
+                n_layers=2,
+                n_dim=self.n_dim,
+            )
+        # otherwise, make it a linear layer
+        else:
+            self.lifting = MLP(
+                in_channels=in_channels,
+                out_channels=self.hidden_channels,
+                hidden_channels=self.hidden_channels,
+                n_layers=1,
+                n_dim=self.n_dim,
+            )
+        
+        self.projection = MLP(
+            in_channels=self.hidden_channels,
+            out_channels=out_channels,
+            hidden_channels=self.projection_channels,
+            n_layers=2,
+            n_dim=self.n_dim,
+            non_linearity=non_linearity,
+        )
 
-    def forward(self, x):
+
+        # Spectral convolution layers
+        self.convs = SpectralConv(
+            self.in_channels,
+            self.out_channels,
+            self.n_modes,
+            max_n_modes=n_modes,
+            n_layers=n_layers,
+            fft_norm=fft_norm
+        )
+
+        self.fno_skips = nn.ModuleList(
+            [
+                getattr(nn, f"Conv{self.n_dim}d")(
+                    in_channels=self.in_features,
+                    out_channels=self.out_features,
+                    kernel_size=1,
+                    bias=skip_fno_bias,
+                )
+                for _ in range(n_layers)
+            ]
+        )
+
+    def forward(self, x, output_shape=None, **kwargs):
+        """TFNO's forward pass
+
+        Parameters
+        ----------
+        x : tensor
+            input tensor
+        output_shape : {tuple, tuple list, None}, default is None
+            Gives the option of specifying the exact output shape for odd shaped inputs.
+            * If None, don't specify an output shape
+            * If tuple, specifies the output-shape of the **last** FNO Block
+            * If tuple list, specifies the exact output-shape of each FNO Block
         """
-        FNO forward pass for single batch
-        
-        input shape: (batchsize, x1, y2, x3, p=1)
-        output shape: (batchsize, x1, y2, x3, p=1)
-        """
-        batchsize = x.shape[0]
-        size_x, size_y, size_z = x.shape[1], x.shape[2], x.shape[3]
-        
-        x = self.upscale(x)
-        x = x.permute(0, 4, 1, 2, 3)
 
-        for i in range(self.n_blocks):
-            x1 = self.spectral_conv3d[i](x)
-            x2 = self.linear[i](x.view(batchsize, self.width, -1)).view(batchsize, self.width,
-                                                                        size_x, size_y, size_z)
-            x = x1 + x2
-            x = F.relu(x)
-        
-        x = x.permute(0, 2, 3, 4, 1)
-        x = self.downscale(x)
-        x = F.relu(x)
-        x = self.output(x)
-        
+        if output_shape is None:
+            output_shape = [None]*self.n_layers
+        elif isinstance(output_shape, tuple):
+            output_shape = [None]*(self.n_layers - 1) + [output_shape]
+
+        x = self.lifting(x)
+
+        for layer_idx in range(self.n_layers):
+            x_skip_fno = self.fno_skips[layer_idx](x)
+            x_fno = self.convs(x, layer_idx, output_shape=output_shape)
+
+            if self.norm is not None:
+                x_fno = self.norm[self.n_norms * layer_idx](x_fno)
+
+            x = x_fno + x_skip_fno
+
+            if layer_idx < (self.n_layers - 1):
+                x = self.non_linearity(x)
+
+        x = self.projection(x)
+
         return x
+
+    @property
+    def n_modes(self):
+        return self._n_modes
+
+    @n_modes.setter
+    def n_modes(self, n_modes):
+        self._n_modes = n_modes
